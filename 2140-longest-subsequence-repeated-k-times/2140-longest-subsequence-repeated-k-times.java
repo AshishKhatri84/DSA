9bd5e7b7724e4e08a9395b2bf4db7ffda6cb5cc1
Ashish Khatri <44234ashish@gmail.com>
2025-03-07 09:24:11 +0530

    Time: 44 ms (100%), Space: 43.6 MB (100%) - LeetHub

--- 2140-longest-subsequence-repeated-k-times/2140-longest-subsequence-repeated-k-times.java
+++ 2140-longest-subsequence-repeated-k-times/2140-longest-subsequence-repeated-k-times.java
@@ -1,70 +1,46 @@
 class Solution {
+    private char[] s;
+    private int k;
+    private int[] count = new int[26];
+    private String max = "";
+
     public String longestSubsequenceRepeatedK(String s, int k) {
-        int n = s.length(), maxLen = n/k, maxFreq = 0;
-        int[] sNums = new int[n];
-        for(int i = 0; i < n; i++) sNums[i] = s.charAt(i)-'a';
-
-        int[] cnt = new int[26];
-        for(int i = 0; i < n; i++) maxFreq = Math.max(maxFreq, ++cnt[sNums[i]]);
-        if(maxFreq < k) return "";
-        int validLettersCnt = 0;
-        for(int f : cnt) if(f >= k) validLettersCnt++;
-        int[] validLetters = new int[validLettersCnt];
-        int validLetterIndex = 0;
-
-        for(int i = 25; i >= 0; i--) if(cnt[i] >= k) validLetters[validLetterIndex++] = i;
-        List<Long>[] seq = new ArrayList[maxLen + 1];
-        for(int i = 1; i <= maxLen; i++) seq[i] = new ArrayList<>();
-
-        for(int i = 0; i < 26; i++) {
-            long seqLong = i+1;
-            for(int mult = 1; cnt[i] >= k*mult; mult++) {
-                seq[mult].add(seqLong);
-                seqLong = ((seqLong << 5) | (i+1));
+        this.s = s.toCharArray();
+        this.k = k;
+        for (char c : this.s) count[c - 'a']++;
+        fn(new char[8], 0);
+        return max;
+    }
+    private void fn(char[] curr, int sz) {
+        for (int c = 25; c >= 0; c--) {
+            if (count[c] >= k) {
+                //curr.append((char) ('a' + c));
+                curr[sz++] = (char) ('a' + c);
+                if (isSubsequence(curr, sz)) {
+                    if (sz > max.length()) {
+                        max = new String(curr, 0, sz);
+                    }
+                    count[c] -= k;
+                    fn(curr, sz);
+                    count[c] += k;
+                }
+                sz--;
             }
         }
-        int len = 2;
-        while(len <= maxLen) {
-            for(long prevSeq : seq[len-1]) {
-                int[] seqCnts = new int[26];
-                long prevItr = prevSeq;
-                while(prevItr > 0) {
-                    seqCnts[(int)(prevItr & 31) - 1]+=k;
-                    prevItr >>= 5;
-                }
-                for(int validLetter : validLetters) {
-                    if(cnt[validLetter] == seqCnts[validLetter]) continue;
-                    long newSeq = prevSeq | ((validLetter+1L) << (5*(len-1)));
-                    if(hasKRepeats(sNums, newSeq, k)) seq[len].add(newSeq);   
+    }
+    private boolean isSubsequence(char[] t, int sz) {
+        if (sz == 1) return true;
+        int pos = 0, cnt = 0;
+        for (char c : s) {
+            if (c == t[pos]) {
+                pos++;
+                if (pos == sz) {
+                    cnt++;
+                    if (cnt == k) return true;
+                    pos = 0;
                 }
             }
-            if(seq[len].isEmpty()) break;
-            len++;
         }
-        long maxLongSeq = 0;
-        for(long seqLong : seq[len-1]) maxLongSeq = Math.max(seqLong, maxLongSeq);
-        return longToString(maxLongSeq);
-    }
-
-    private String longToString(long num) {
-        StringBuilder sb = new StringBuilder();
-        while(num > 0) {
-            sb.append((char)('a' + (num & 31) - 1));
-            num >>= 5;
-        }
-        return sb.reverse().toString();
-    }
-    private boolean hasKRepeats(int[] sNums, long subseq, int k) {
-        long currSubseq = subseq;
-        int repeats = 0;
-        for(int i = sNums.length-1; i >= 0 && repeats < k; i--) {
-            if(sNums[i] != (currSubseq & 31)-1) continue;
-            currSubseq >>= 5;
-            if(currSubseq == 0) {
-                currSubseq = subseq;
-                repeats++;
-            }
-        }
-        return repeats == k;
+        return false;
     }
 }
