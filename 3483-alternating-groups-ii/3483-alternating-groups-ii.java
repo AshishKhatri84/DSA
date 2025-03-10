6e89496409468cf775462a11c8777ae0b87bfb1b
Ashish Khatri <44234ashish@gmail.com>
2025-03-10 17:02:08 +0530

    Time: 2 ms (98.05%), Space: 63.7 MB (8.91%) - LeetHub

--- 3483-alternating-groups-ii/3483-alternating-groups-ii.java
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.java
@@ -1,29 +1,18 @@
-public class Solution {
+class Solution {
     public int numberOfAlternatingGroups(int[] colors, int k) {
-        int[] extendedColors = new int[colors.length + k - 1];
-        for (int i = 0; i < colors.length; i++) {
-            extendedColors[i] = colors[i];
-        }
-        for (int i = 0; i < k - 1; i++) {
-            extendedColors[colors.length + i] = colors[i];
-        }
-        int length = extendedColors.length;
-        int result = 0;
-        int left = 0;
-        int right = 1;
-
-        while (right < length) {
-            if (extendedColors[right] == extendedColors[right - 1]) {
-                left = right;
-                right++;
-                continue;
+        int cnt = 1;
+        int res = 0;
+        int firstcnt = 0;
+        for (int i = 1; i < colors.length; i++) {
+            if (colors[i] != colors[i-1]) cnt++;
+            else {
+                if (firstcnt == 0) firstcnt = Math.min(k-1, cnt); 
+                cnt = 1;
             }
-            right++;
-
-            if (right - left < k) continue;
-            result++;
-            left++;
+            if (cnt >= k) res++;
         }
-        return result;
+        if (colors[colors.length-1] == colors[0]) cnt = -1000;
+        if (cnt == colors.length) return cnt;
+        return res + Math.max(0, firstcnt + Math.min(cnt, k-1) - k + 1);
     }
 }
