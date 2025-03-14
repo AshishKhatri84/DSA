e1236cbd9f540f4c90e10dae2a2323cfeb8acf80
Ashish Khatri <44234ashish@gmail.com>
2025-03-14 23:23:59 +0530

    Time: 11 ms (98.79%), Space: 63.2 MB (45.97%) - LeetHub

--- 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.java
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.java
@@ -1,37 +1,30 @@
 class Solution {
+    public boolean checkvalid(int []candie,int val,long m){
+    long count=0;
+    for(int k=0;k<candie.length;k++){
+        count=count+candie[k]/val;
+    }
+    return count>=m;
+    }
     public int maximumCandies(int[] candies, long k) {
-        int maxCandiesInPile = 0;
-        for (int i = 0; i < candies.length; i++) {
-            maxCandiesInPile = Math.max(maxCandiesInPile, candies[i]);
-        }
-
-        int left = 0;
-        int right = maxCandiesInPile;
-
-        while (left < right) {
-            int middle = (left + right + 1) / 2;
-
-            if (canAllocateCandies(candies, k, middle)) {
-                left = middle;
-            } else {
-                right = middle - 1;
+long sum=0;
+int min=1;
+for(int i=0;i<candies.length;i++){
+   sum=sum+candies[i];
+}
+if(sum<k){
+    return 0;
+}
+int max=(int) (sum/k);
+        while(min<=max){
+            int mid = (min+max)/2;
+            if(checkvalid(candies,mid,k)){
+                min=mid+1; 
+            }
+            else{
+                max=mid-1;
             }
         }
-
-        return left;
+        return max;
     }
-
-    private boolean canAllocateCandies(
-        int[] candies,
-        long k,
-        int numOfCandies
-    ) {
-        long maxNumOfChildren = 0;
-
-        for (int pileIndex = 0; pileIndex < candies.length; pileIndex++) {
-            maxNumOfChildren += candies[pileIndex] / numOfCandies;
-        }
-
-        return maxNumOfChildren >= k;
-    }
-}
+} 
