ab90ad945cf228cb900dafb3c28e4f49d721a4a0
Ashish Khatri <44234ashish@gmail.com>
2025-03-18 08:41:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Partition Equal Subset Sum/partition-equal-subset-sum.java
+++ Difficulty: Medium/Partition Equal Subset Sum/partition-equal-subset-sum.java
@@ -30,37 +30,20 @@ class GFG {
 // } Driver Code Ends
 
 
-// User function Template for Java
-
 class Solution {
-    public boolean equalPartition(int arr[]) {
-        return canPartition(arr);
-    }
-
-    private boolean canPartition(int[] nums) {
-        int total = 0;
-        int n = nums.length;
-        for (int num : nums) {
-            total += num;
-        }
+    public boolean equalPartition(int[] arr) {
+        int total = Arrays.stream(arr).sum();
         if (total % 2 != 0) return false;
 
         int subsetSum = total / 2;
-        Boolean[][] dp = new Boolean[n][subsetSum + 1];
+        boolean[] dp = new boolean[subsetSum + 1];
+        dp[0] = true; 
 
-        return findSubset(dp, nums, n - 1, subsetSum);
-    }
-
-    private boolean findSubset(Boolean[][] dp, int[] nums, int index, int subsetSum) {
-        if (subsetSum == 0) return true;
-        if (index < 0 || subsetSum < 0) return false;
-
-        if (dp[index][subsetSum] != null) return dp[index][subsetSum];
-
-        boolean include = findSubset(dp, nums, index - 1, subsetSum - nums[index]);
-        boolean exclude = findSubset(dp, nums, index - 1, subsetSum);
-
-        dp[index][subsetSum] = include || exclude;
-        return dp[index][subsetSum];
+        for (int num : arr) {
+            for (int j = subsetSum; j >= num; j--) {
+                dp[j] = dp[j] || dp[j - num]; 
+            }
+        }
+        return dp[subsetSum];
     }
 }
