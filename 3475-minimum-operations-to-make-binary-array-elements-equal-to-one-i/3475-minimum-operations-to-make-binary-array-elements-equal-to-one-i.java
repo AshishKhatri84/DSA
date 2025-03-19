1fff99f2e13f37677360a313bbb4dcd05356ef4a
Ashish Khatri <44234ashish@gmail.com>
2025-03-19 10:02:51 +0530

    Time: 7 ms (66.53%), Space: 57.3 MB (74.95%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
@@ -1,23 +1,17 @@
 class Solution {
-
     public int minOperations(int[] nums) {
-        Deque<Integer> flipQueue = new ArrayDeque<>();
         int count = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            while (!flipQueue.isEmpty() && i > flipQueue.peekFirst() + 2) {
-                flipQueue.pollFirst();
-            }
-
-            if ((nums[i] + flipQueue.size()) % 2 == 0) {
-                if (i + 2 >= nums.length) {
-                    return -1;
-                }
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i - 2] == 0) {
                 count++;
-                flipQueue.offerLast(i);
+                nums[i - 2] = nums[i - 2] ^ 1;
+                nums[i - 1] = nums[i - 1] ^ 1;
+                nums[i] = nums[i] ^ 1;
             }
         }
-
-        return count;
+        int sum = 0;
+        for (int num : nums) sum += num;
+        if (sum == nums.length) return count;
+        return -1;
     }
 }
