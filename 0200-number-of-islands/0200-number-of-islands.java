e6b587d088d4f81bc9c34bc7e8de4336d91a6778
Ashish Khatri <44234ashish@gmail.com>
2025-04-05 09:50:34 +0530

    Time: 1 ms (100%), Space: 49.6 MB (60.62%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,33 +1,32 @@
 class Solution {
-    private static void dfs(int i,int j,int m,int n,char[][] grid)
-    {
-        if(i<0 || i>=m || j<0 || j>=n) return;
-        if(grid[i][j]=='1'){
-        grid[i][j]='#';
-        
+    int rows, cols, islands = 0;
 
-        dfs(i-1,j,m,n,grid);
-        dfs(i,j-1,m,n,grid);
-        dfs(i+1,j,m,n,grid);
-        dfs(i,j+1,m,n,grid);
-        }
-        else return;
-    }
     public int numIslands(char[][] grid) {
-        int ans =0;
-        int m = grid.length;
-        int n = grid[0].length;
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(grid[i][j]=='1')
-                {
-                    dfs(i,j,m,n,grid);
-                    ans++;
-                }
+        rows = grid.length;
+        cols = grid[0].length;
+        for (int row = 0; row < rows; row++)
+            check(grid, row);
+        return islands;
+    }
+
+    public void check(final char[][] grid, int row) {
+        final char[] finalRow = grid[row];
+        for (int col = 0; col < cols; ++col)
+            if (finalRow[col] == '1') {
+                bfs(grid, row, col);
+                ++islands;
             }
-        }
-        return ans;
+    }
+
+    public void bfs(char[][] grid, int row, int col) {
+        grid[row][col] = '*';
+        if (row > 0 && grid[row - 1][col] == '1')
+            bfs(grid, row - 1, col);
+        if (row + 1 < rows && grid[row + 1][col] == '1')
+            bfs(grid, row + 1, col);
+        if (col > 0 && grid[row][col - 1] == '1')
+            bfs(grid, row, col - 1);
+        if (col + 1 < cols && grid[row][col + 1] == '1')
+            bfs(grid, row, col + 1);
     }
 }
