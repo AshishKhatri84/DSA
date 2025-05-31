e6a8e180b59d88c7de3a46292459d49e2de2db24
Ashish Khatri <44234ashish@gmail.com>
2025-05-31 08:51:51 +0530

    Time: 4 ms (100%), Space: 58.8 MB (76.65%) - LeetHub

--- 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.java
+++ 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.java
@@ -1,36 +1,28 @@
 class Solution {
-    public void dfs(int node, int[] edges, int[] dist, Boolean[] visit) {
-        visit[node] = true;
-        int neighbor = edges[node];
-        if (neighbor != -1 && !visit[neighbor]) {
-            dist[neighbor] = 1 + dist[node];
-            dfs(neighbor, edges, dist, visit);
-        }
-    }
-
     public int closestMeetingNode(int[] edges, int node1, int node2) {
         int n = edges.length;
-        int[] dist1 = new int[n], dist2 = new int[n];
-        Arrays.fill(dist1, Integer.MAX_VALUE);
-        Arrays.fill(dist2, Integer.MAX_VALUE);
-        dist1[node1] = 0;
-        dist2[node2] = 0;
-
-        Boolean[] visit1 = new Boolean[n], visit2 = new Boolean[n];
-        Arrays.fill(visit1, Boolean.FALSE);
-        Arrays.fill(visit2, Boolean.FALSE);
-
-        dfs(node1, edges, dist1, visit1);
-        dfs(node2, edges, dist2, visit2);
-
-        int minDistNode = -1, minDistTillNow = Integer.MAX_VALUE;
-        for (int currNode = 0; currNode < n; currNode++) {
-            if (minDistTillNow > Math.max(dist1[currNode], dist2[currNode])) {
-                minDistNode = currNode;
-                minDistTillNow = Math.max(dist1[currNode], dist2[currNode]);
+        boolean[] v1 = new boolean[n];
+        boolean[] v2 = new boolean[n];
+        int ans = Integer.MAX_VALUE;
+        while (node1 != -1 || node2 != -1) {
+            if (node1 != -1) {
+                if (v2[node1]) ans = Math.min(ans, node1);
+                if (v1[node1]) node1 = -1;
+                else {
+                    v1[node1] = true;
+                    node1 = edges[node1];
+                }
             }
+            if (node2 != -1) {
+                if (v1[node2]) ans = Math.min(ans, node2);
+                if (v2[node2]) node2 = -1;
+                else {
+                    v2[node2] = true;
+                    node2 = edges[node2];
+                }
+            }
+            if (ans != Integer.MAX_VALUE) return ans;
         }
-
-        return minDistNode;
+        return -1;
     }
 }
