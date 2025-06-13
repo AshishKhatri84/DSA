34c3d2cdf10bf8426afafafc6d3c75d973ca8768
Ashish Khatri <44234ashish@gmail.com>
2025-06-13 22:10:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Koko Eating Bananas/koko-eating-bananas.java
+++ Difficulty: Medium/Koko Eating Bananas/koko-eating-bananas.java
@@ -1,65 +1,30 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
-
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-
-            // Create an ArrayList to store the integers
-            ArrayList<Integer> array = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                array.add(Integer.parseInt(token));
-            }
-
-            int[] arr = new int[array.size()];
-            int idx = 0;
-            for (int i : array) arr[idx++] = i;
-
-            int k = Integer.parseInt(br.readLine());
-            // Create Solution object and find closest sum
-            Solution ob = new Solution();
-            int ans = ob.kokoEat(arr, k);
-            System.out.println(ans);
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
-    public static int kokoEat(int[] arr, int k) {
-    int n = arr.length;
-        long total = 0;
-        for (int p : arr) 
-            total += p;
-        
-        int left = (int) ((total - 1) / k) + 1;
-        int right = (int) ((total - n) / (k - n + 1)) + 1;
-        
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            int time = 0;
-            for (int p : arr) {
-                time += (p - 1) / mid + 1;
-            }
-            if (time > k) {
-                left = mid + 1;
+    public int kokoEat(int[] arr, int k) {
+        int low = 1, high = 0;
+
+        for (int pile : arr) {
+            high = Math.max(high, pile);
+        }
+
+        int answer = high;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (canFinish(arr, k, mid)) {
+                answer = mid;
+                high = mid - 1; 
             } else {
-                right = mid;
+                low = mid + 1;  
             }
         }
-        return left;
+        return answer;
+    }
+
+    private boolean canFinish(int[] arr, int k, int s) {
+        int hours = 0;
+        for (int pile : arr) {
+            hours += (pile + s - 1) / s; 
+        }
+        return hours <= k;
     }
 }
