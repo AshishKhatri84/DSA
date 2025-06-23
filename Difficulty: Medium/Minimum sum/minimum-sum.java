2de8de9eee1a8c73f0e51ff815257f6dc3723003
Ashish Khatri <44234ashish@gmail.com>
2025-06-23 18:29:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum sum/minimum-sum.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+    String minSum(int[] arr) {
+        Arrays.sort(arr);
+        StringBuilder num1 = new StringBuilder();
+        StringBuilder num2 = new StringBuilder();
+        
+        // Distribute digits alternatively to two numbers
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 0)
+                num1.append(arr[i]);
+            else
+                num2.append(arr[i]);
+        }
+
+        // Remove leading zeros while adding
+        return addStrings(num1.toString(), num2.toString());
+    }
+
+    // Adds two numbers represented as strings
+    private String addStrings(String num1, String num2) {
+        StringBuilder result = new StringBuilder();
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int digit1 = i >= 0 ? num1.charAt(i--) - '0' : 0;
+            int digit2 = j >= 0 ? num2.charAt(j--) - '0' : 0;
+
+            int sum = digit1 + digit2 + carry;
+            result.append(sum % 10);
+            carry = sum / 10;
+        }
+
+        // Reverse and remove leading zeros from the result
+        String finalResult = result.reverse().toString();
+        int index = 0;
+        while (index < finalResult.length() && finalResult.charAt(index) == '0') {
+            index++;
+        }
+
+        return index == finalResult.length() ? "0" : finalResult.substring(index);
+    }
+}
