6a1ef842eac0fa84f6868427af3e0be9a9e6afaf
Ashish Khatri <44234ashish@gmail.com>
2026-03-17 19:37:05 +0530

    Time: 111 ms (93.55%), Space: 33.6 MB (92.47%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.py
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.py
@@ -1,26 +1,15 @@
 class Solution:
-    def largestSubmatrix(self, matrix: List[List[int]]) -> int:
-        m = len(matrix)
-        n = len(matrix[0])
-        prev_heights = []
-        ans = 0
+    def largestSubmatrix(self, matrix: list[list[int]]) -> int:
+        if not matrix: 
+            return 0
+        m, n = len(matrix), len(matrix[0])
+        heights = [0] * n
+        max_area = 0
 
-        for row in range(m):
-            heights = []
-            seen = [False] * n
-            
-            for height, col in prev_heights:
-                if matrix[row][col] == 1:
-                    heights.append((height + 1, col))
-                    seen[col] = True
-
-            for col in range(n):
-                if seen[col] == False and matrix[row][col] == 1:
-                    heights.append((1, col))
-            
-            for i in range(len(heights)):
-                ans = max(ans, heights[i][0] * (i + 1))
-                
-            prev_heights = heights
-
-        return ans
+        for row in matrix:
+            for j in range(n):
+                heights[j] = heights[j] + 1 if row[j] else 0
+            sorted_heights = sorted(heights, reverse=True)
+            for j in range(n):
+                max_area = max(max_area, sorted_heights[j] * (j + 1))
+        return max_area
